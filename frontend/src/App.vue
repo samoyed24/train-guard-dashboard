142624397741a5ae6c72b985e1a6940d4c391883
qwe15526 <2478769631@qq.com>
2026-03-21 15:47:40 +0800

    fix(frontend): replace native scrollbar with Element Plus scrollbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <router-view v-slot="{ Component, route }">
-    <transition name="page-fade-slide" mode="out-in">
-      <component :is="Component" :key="route.matched[0]?.path || route.path" />
-    </transition>
-  </router-view>
+  <el-scrollbar height="100vh">
+    <router-view v-slot="{ Component, route }">
+      <transition name="page-fade-slide" mode="out-in">
+        <component :is="Component" :key="route.matched[0]?.path || route.path" />
+      </transition>
+    </router-view>
+  </el-scrollbar>
 </template>
